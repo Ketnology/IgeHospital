94f1e5b545a938847e8d63572edfa856f127c6f9
mosesket <46220123+mosesket@users.noreply.github.com>
2025-03-18 22:03:36 +0100

    Update api_endpoints.dart

--- lib/constants/api_endpoints.dart
+++ lib/constants/api_endpoints.dart
@@ -1,5 +1,5 @@
 class ApiEndpoints {
-  static const String baseUrl = "http://127.0.0.1:8000/api";
+  static const String baseUrl = "https://ige.myketnology.com/api";
 
   static const String login = "$baseUrl/admin/login";
   static const String logout = "$baseUrl/admin/logout";
