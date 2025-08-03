44cfbabf722860d3a0515b2d3dc174a5dabe6f74
Moses Ketuojo <sirketxp@gmail.com>
2025-08-03 11:45:56 +0100

    vital signs

--- lib/controllers/auth_controller.dart
+++ lib/controllers/auth_controller.dart
@@ -58,8 +58,8 @@ class AuthController extends GetxController {
       try {
         Get.log("AuthController - Attempting login with email: $emailController.text");
 
-        // await authService.login(emailController.text, passwordController.text);
-        await authService.login('igehospital@gmail.com', 'password');
+        await authService.login(emailController.text, passwordController.text);
+        // await authService.login('igehospital@gmail.com', 'password');
         // await authService.login('nurse@gmail.com', 'password'); // nurse
         // await authService.login('doctor@gmail.com', 'password'); // doctor
         // await authService.login('patient@gmail.com', 'password');
